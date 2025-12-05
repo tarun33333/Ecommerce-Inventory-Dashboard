9f6bc375ec7249f0c1a49285f16ba4fa95219b85
tarun33333 <tarunvikram333@gmail.com>
2025-12-05 15:51:56 +0530

    tasks 3 completedd

--- frontend/src/Components/ManagerDashboard.jsx
+++
@@ -1,8 +0,0 @@
-export default function ManagerDashboard() {
-      return (
-            <div className="dashboard-container">
-                  <h2 className="dashboard-title">Manager Dashboard</h2>
-                  <p className="dashboard-text">Add/edit products, manage stock.</p>
-            </div>
-      );
-}
--- frontend/src/Components/StaffDashboard.jsx
+++
@@ -1,8 +0,0 @@
-export default function StaffDashboard() {
-    return (
-        <div className="dashboard-container">
-            <h2 className="dashboard-title">Staff Dashboard</h2>
-            <p className="dashboard-text">View products & update stock quantity only.</p>
-        </div>
-    );
-}
